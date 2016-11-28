475491e29aafe6a1550c05c3242d2af6cc244841
Brad Chao <brad@brad.tw>
2016-11-28 11:03:30 +0800

    v4

--- app/src/main/java/com/example/user/brad18/MainActivity.java
+++ app/src/main/java/com/example/user/brad18/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.user.brad18;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,18 +11,25 @@ import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     private SeekBar seekbar;
+    private MyReceiver myReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         seekbar = (SeekBar)findViewById(R.id.seekbar);
+        myReceiver = new MyReceiver();
+        registerReceiver(myReceiver, new IntentFilter("brad"));
     }
 
     @Override
     public void finish() {
         Intent it = new Intent(this, MyService.class);
         stopService(it);
+
+        unregisterReceiver(myReceiver);
+
         super.finish();
     }
 
@@ -33,5 +43,15 @@ public class MainActivity extends AppCompatActivity {
     public void stop(View v){
         Intent it = new Intent(this, MyService.class);
         stopService(it);
+    }
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len", -1);
+            if (len>0){
+                seekbar.setMax(len);
+            }
+        }
     }
 }
